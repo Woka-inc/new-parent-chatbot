84c03b76247814526278c1eec21a31015bf18c67
YunHa <pgup10753@gmail.com>
2024-09-05 13:19:44 +0900

    feat: 데이터 수집, 전처리 기능 추가

---
+++ app.py
@@ -0,0 +1,12 @@
+from data_processor import process_healthychildren_articles
+from crawler import scrape_conditions
+
+condition_url = 'https://www.healthychildren.org/English/health-issues/conditions/'
+base_url = 'https://www.healthychildren.org'
+original_article_path = './res/scraped_condition_articles.json'
+cleaned_article_path = './res/articles.json'
+
+if __name__ == "__main__":
+    # 데이터 수집, 전처리
+    scrape_conditions(base_url, condition_url, path_to_save=original_article_path)
+    process_healthychildren_articles(original_article_path, cleaned_article_path)
